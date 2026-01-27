1d7609c36de40fda17230e1ee2f810802217c5a8
Pedro Silveira <pedrohpss@gmail.com>
2026-01-27 22:13:42 +0000

    fix: Correct typo in skills JSON and enhance stats formatting in string formatter

--- streetrat_creator/string_formatter.py
+++ streetrat_creator/string_formatter.py
@@ -6,7 +6,7 @@ def format_skills(i_class:dict):
     
     result = ""
     for key, value in i_class.items():
-        result += f"{uppercase_first_letter(key)} : {value}\n"
+        result += f"{uppercase_first_letter(key)} : {value}, "
         
     return result
 
@@ -22,8 +22,27 @@ def format_stats(stats):
     stats_s += f"MOVE: {stats['move']}\n"    
     stats_s += f"BODY: {stats['body']}\n"    
     stats_s += f"EMP: {stats['emp']}\n" 
-    return stats_s   
+    
+    stats_t = f"""
+```
+╔══════╤═════╤═════╤══════╤══════╗
+║ BODY │ REF │ DEX │ MOVE │ WILL ║
+╠══════╪═════╪═════╪══════╪══════╣
+║ {stats['body']}    │ {stats['ref']}   │ {stats['dex']}   │ {stats['move']}    │ {stats['will']}    ║
+╚══════╧═════╧═════╧══════╧══════╝
+
+╔══════╤══════╤══════╤═════╤═════╗
+║ TECH │ COOL │ LUCK │ INT │ EMP ║
+╠══════╪══════╪══════╪═════╪═════╣
+║ {stats['tech']}    │ {stats['cool']}    │ {stats['luck']}    │ {stats['int']}   │ {stats['emp']}   ║
+╚══════╧══════╧══════╧═════╧═════╝
+```
+"""
+
+    return stats_t   
+
+
     
 def string_result(role, hp, humanity, role_skill, stats, skills):
-    result = f"Selected role: {role}\nHP: {hp}\nHumanity: {humanity}\nStats: {format_stats(stats)}\nRole Skill: {role_skill}\n" + f"Skills: \n{format_skills(skills[role.lower()])}"
+    result = f"Selected role: {role}\nHP: {hp}\nHumanity: {humanity}\nStats: {format_stats(stats)}Role Skill: {role_skill}\n" + f"Skills: \n{format_skills(skills[role.lower()])}"
     return result
